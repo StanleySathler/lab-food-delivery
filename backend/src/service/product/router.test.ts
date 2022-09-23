1e881b26a8bd9d335a3f54cb8f6dfdd0f10ec240
StanleySathler <stanleysathlerpinto@gmail.com>
2022-09-23 20:53:47 -0300

    Setup Fastify tests

--- backend/src/service/product/router.test.ts
+++ backend/src/service/product/router.test.ts
@@ -1,3 +1,23 @@
-it('Should work', () => {
-  expect(1).toBe(1);
+import { FastifyInstance } from 'fastify';
+import { buildServer } from '../../server';
+
+let app: FastifyInstance;
+
+beforeAll(async () => {
+  app = buildServer();
+});
+
+afterAll(async () => {
+  await app.close();
+});
+
+describe('List products', () => {
+  it('Should respond 200 and list all projects', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: '/product',
+    });
+
+    expect(response.json()).toStrictEqual([{ id: '1', name: 'Cup Cake' }]);
+  });
 });
