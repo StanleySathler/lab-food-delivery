c2c7eb84ef3d2d0a064cabe83e8fe8dc6fdd18ba
StanleySathler <stanleysathlerpinto@gmail.com>
2022-09-25 18:07:27 -0300

    Make app listen to $PORT

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ const server = buildServer();
 
 const startServer = async () => {
   try {
-    await server.listen({ port: Number(process.env.PORT) || 3000 });
+    await server.listen({ port: Number(process.env.PORT) });
   } catch (err) {
     server.log.error(err);
     process.exit(1);
