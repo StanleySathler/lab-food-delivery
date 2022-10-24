0a05b4ee32af119780c8155601b0ab933eed0ea5
StanleySathler <stanleysathlerpinto@gmail.com>
2022-10-23 21:33:36 -0300

    Refactor to trigger build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,9 @@ import { buildServer } from './server';
 
 const server = buildServer();
 
-console.log('> hihi ', process.env);
-
 const startServer = async () => {
   try {
     await server.listen({ port: Number(process.env.PORT), host: '::' });
-    console.log('> Server listening on port', process.env.PORT);
   } catch (err) {
     server.log.error(err);
     process.exit(1);
