766abf3b30eedffd47cbc4c9bab8142b01c8da36
StanleySathler <stanleysathlerpinto@gmail.com>
2022-10-23 21:23:53 -0300

    Listen to addr 0:0:0:0

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,11 @@ import { buildServer } from './server';
 const server = buildServer();
 
 console.log('> hihi ', process.env);
-console.log('> halo');
 
 const startServer = async () => {
   try {
-    await server.listen({ port: Number(process.env.PORT) });
+    await server.listen({ port: Number(process.env.PORT), host: '::' });
+    console.log('> Server listening on port', process.env.PORT);
   } catch (err) {
     server.log.error(err);
     process.exit(1);
