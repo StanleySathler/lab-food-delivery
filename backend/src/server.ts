3e31b91161cef393cdd2ee089806c1cf6cfec78b
StanleySathler <stanleysathlerpinto@gmail.com>
2023-01-28 22:40:47 -0300

    feat: disable CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,14 @@
 import fastify from 'fastify';
+import cors from '@fastify/cors';
+
 import productRouter from './service/product/router';
 
 export const buildServer = () => {
   const server = fastify({
     logger: true,
   });
+
+  server.register(cors);
 
   server.register(productRouter, { prefix: '/product' });
 
