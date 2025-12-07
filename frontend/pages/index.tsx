cb579db7f588cdf4ac25f965b8beae9031339a61
StanleySathler <stanleysathlerpinto@gmail.com>
2025-12-07 20:06:59 -0300

    revert scroll mobile

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -181,10 +181,10 @@ const Home: NextPage = () => {
 
         {/* Promo Cards Section */}
         <section className="py-6">
-          <div className="max-w-screen-xl mx-auto px-4">
-            <div className="flex flex-row overflow-x-auto gap-4 scroll-snap-x-mandatory">
+          <div className="max-w-screen-xl mx-auto pl-4">
+            <div className="flex flex-row overflow-x-auto gap-4">
               {promos.map((promo) => (
-                <div key={promo.id} className="bg-white rounded-lg shadow-md overflow-hidden w-full md:w-1/3 flex-shrink-0 scroll-snap-align-start">
+                <div key={promo.id} className="bg-white rounded-lg shadow-md overflow-hidden w-full flex-shrink-1 scroll-snap-align-start">
                   <img src={promo.image} alt="Promo" className="w-full h-32 object-cover" />
                   {/* <div className="p-4">
                     <h3 className="font-semibold text-lg">{promo.title}</h3>
