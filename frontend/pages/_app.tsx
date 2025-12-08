feb199dbec1b1dc9368436bf29f36af956852174
StanleySathler <stanleysathlerpinto@gmail.com>
2025-12-08 07:22:24 -0300

    add msw

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,19 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
+      import('../src/mocks/browser').then(({ worker }) => {
+        worker.start();
+      });
+    }
+  }, []);
+
   return (
     <>
       <Head>
