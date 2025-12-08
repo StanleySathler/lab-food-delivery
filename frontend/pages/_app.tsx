5b6766c3f8f67621dbd4fc7da60669f0369c86b0
StanleySathler <stanleysathlerpinto@gmail.com>
2025-12-08 07:34:45 -0300

    enable msw on prod

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -7,7 +7,8 @@ const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
+    // if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
+    if (typeof window !== 'undefined') {
       import('../src/mocks/browser').then(({ worker }) => {
         worker.start();
       });
