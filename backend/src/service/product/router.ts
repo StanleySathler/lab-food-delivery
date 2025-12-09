e26f1debb866473a38dd4041e9ff9946ee71991e
Stanley Sathler <stanleysathlerpinto@gmail.com>
2025-12-09 07:22:38 -0300

    chore: Bunch of improvements (see desc) (#31)
    * remove backend code
    
    * add frontend-main workflow
    
    * update readme

--- backend/src/service/product/router.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from 'fastify';
-import * as controller from './controller';
-import { listProductsSchema } from './schema';
-
-export default async function productRouter(fastify: FastifyInstance) {
-  fastify.get('/', { schema: listProductsSchema }, controller.listProducts);
-}
