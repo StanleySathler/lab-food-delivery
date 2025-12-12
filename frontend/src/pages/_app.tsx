d266468b1c4f89961ba5dd922a5a349e54a94411
Stanley Sathler <stanleysathlerpinto@gmail.com>
2025-12-12 10:35:57 -0300

    frontend: Add OG data (#41)

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -10,6 +10,14 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>FooDelivery - Food Delivery App</title>
+        <meta name="description" content="Order delicious food from your favorite restaurants with fast delivery." />
+        <meta property="og:title" content="FooDelivery - Food Delivery App" />
+        <meta property="og:description" content="Order delicious food from your favorite restaurants with fast delivery." />
+        <meta property="og:image" content="/icon.png" />
+        <meta property="og:url" content="https://lab-food-delivery.vercel.app/" />
+        <meta name="twitter:card" content="summary_large_image" />
+
         {/* TODO: Don't use CDN but a PostCSS alternative. */}
         <script src="https://cdn.tailwindcss.com"></script>
       </Head>
